947339b93a96cff057e1c5f63cd428e867fb29f6
Heikki000 <heikki.savolainen2@metropolia.fi>
2024-02-07 14:51:03 +0200

    lisätty tapahtumatyypit

--- Simulator/src/simu/model/TapahtumanTyyppi.java
+++ Simulator/src/simu/model/TapahtumanTyyppi.java
@@ -5,6 +5,8 @@ import simu.framework.ITapahtumanTyyppi;
 // TODO:
 // Tapahtumien tyypit määritellään simulointimallin vaatimusten perusteella
 public enum TapahtumanTyyppi implements ITapahtumanTyyppi{
-	ARR1, DEP1, DEP2, DEP3;
+
+	//S = Saapuminen, P = Poistuminen
+	AULA_S, AULA_P, INFO_S, INFO_P, KAUPPA_S, KAUPPA_P, RESEPTI_S, RESEPTI_P, KASSA_S, KASSA_P;
 
 }
