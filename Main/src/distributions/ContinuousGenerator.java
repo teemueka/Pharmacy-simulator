baed6a0fdf4b0379557bce1ebcaa400225059130
JoelPalu <kirill.saveliev@metropolia.fi>
2024-02-07 13:21:15 +0200

    Revert "dir fix"
    This reverts commit aa8451ce59514b385a198efd137194a4b3bdf015.

--- Main/src/distributions/ContinuousGenerator.java
+++
@@ -1,7 +0,0 @@
-package distributions;
-
-/** A continuous generator provides a double value according to the distribution it relies on.
- */
-public interface ContinuousGenerator extends Seedable {
-    double sample(); 
-}
