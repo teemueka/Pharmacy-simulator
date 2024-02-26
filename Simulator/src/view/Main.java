9916fb51e7f514d3c09064de8546e87ae9fe25b5
Heikki <heikki.savolainen2@metropolia.fi>
2024-02-26 17:40:51 +0200

    oman branchin testailua

--- Simulator/src/view/Main.java
+++ Simulator/src/view/Main.java
@@ -1,7 +1,7 @@
 package view;
 
 public class Main {
-    // JavaFX-sovelluksen (käyttöliittymän) käynnistäminen
+    // JavaFX-sovelluksen (käyttöliittymän) käynnistä5minen
     public static void main(String[] args) {
         SimulaattorinGUI.launch(SimulaattorinGUI.class);
     }
