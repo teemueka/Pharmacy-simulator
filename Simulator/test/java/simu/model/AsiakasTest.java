c96a01ef1f0e3d9837b42f851b98fee843583326
Heikki <heikki.savolainen2@metropolia.fi>
2024-02-26 18:46:26 +0200

    Asiakas-luokan testien tekemistä aloitettu

---
+++ Simulator/test/java/simu/model/AsiakasTest.java
@@ -0,0 +1,80 @@
+package simu.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AsiakasTest {
+
+    @Test
+    void getNextService() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hasMoreServices() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setKokonaisPalveluaika() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPalveluaika() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getKaikkiPalveluAjat() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPoistumisaika() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPoistumisaika() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getSaapumisaika() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setSaapumisaika() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getId() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAspaKäyty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getKauppaKäyty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getReseptiKäyty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setAspaKäyty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setKauppaKäyty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setReseptiKäyty() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void displayUsedServices() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void raportti() {
+    }
+}
